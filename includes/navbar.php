bf5af43489bf16f081816206977509e33bc78103
JoaoVittorVenturini <joaovittorventurini@outlook.com>
2023-10-02 17:01:59 -0300

    fix navbar

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,7 +1,32 @@
-<header class="d-flex justify-content-center align-items-center w-100 bg-green" style="height: 5.5rem;">
-  <a href="\estoque\index.php">
-    <img src="\estoque\img\logo.svg" alt="Logo Esquadrilon" class="w-75">
-  </a>
+<header class="d-flex justify-content-center align-items-center w-100 bg-green" style="height: 4rem;">
+  <nav class="navbar navbar-expand-lg">
+    <ul class="navbar-nav fs-4">
+      <li class="nav-item mx-2">
+        <a class="nav-link text-white-50" href="\estoque\pages\Clientes\read.php"><i class="bi bi-person"></i> Clientes</a>
+      </li>
+      <li class="nav-item mx-4">
+        <a class="nav-link text-white-50" href="\estoque\pages\Obras\read.php"><i class="bi bi-buildings"></i> Obras</a>
+      </li>
+      <li class="nav-item mx-4">
+        <a class="nav-link text-white-50" href="\estoque\pages\Perfis\read.php"><i class="bi bi-slash-lg"></i> Perfis</a>
+      </li>
+      <li class="nav-item mx-4">
+        <a class="nav-link text-white-50" href="\estoque\pages\Cores\read.php"><i class="bi bi-palette"></i> Cores</a>
+      </li>
+      <li class="nav-item mx-4">
+        <a class="nav-link text-white-50" href="\estoque\pages\Entradas\read.php"><i class="bi bi-plus-lg"></i> Entradas</a>
+      </li>
+      <li class="nav-item mx-4">
+        <a class="nav-link text-white-50" href="\estoque\pages\Saidas\read.php"><i class="bi bi-dash-lg"></i> Saídas</a>
+      </li>
+      <li class="nav-item mx-4">
+        <a class="nav-link text-white-50" href="\estoque\pages\estoque.php"><i class="bi bi-inboxes"></i> Ver estoque</a>
+      </li>
+    </ul>
+  </nav>
 </header>
 
-<!-- https://afeal.com.br/rev/wp-content/uploads/2020/01/LOGO-ESQUADRILON.png -->
+
+<!-- <a href="\estoque\index.php">
+  <img src="\estoque\img\logo.svg" alt="Logo Esquadrilon" class="w-75">
+</a> -->
