aceefe37c9a4d1530e04d29896d2fc90dcb98f10
JoaoVittorVenturini <joaovittorventurini@outlook.com>
2023-10-06 15:59:35 -0300

    test

--- pages/Saidas/controller.php
+++ pages/Saidas/controller.php
@@ -17,34 +17,50 @@ foreach ($data as $input_name) {
         ? ${$input_name} = $_POST[$input_name]
         : ${$input_name} = "";
 };
-  
+
+
+
 $redirect_success = "./read.php";
 $redirect_error = "./read.php";
 
 try {
   switch ($_REQUEST["acao"]) {
     case 'cadastrar':
+        $perfil = isset($_POST['perfil']) ? $perfil = $_POST['perfil'] : [];
+        $tamanho = isset($_POST['tamanho']) ? $_POST['tamanho'] : [];
+        $cor = isset($_POST['cor_id']) ? $_POST['cor_id'] : [];
+        $quantidade = isset($_POST['quantidade']) ? $_POST['quantidade'] : [];
 
-        $perfil = $_POST['perfil'];
-        $tamanho = $_POST['tamanho'];
-        $cor = $_POST['cor_id'];
-        $quantidade = $_POST['quantidade'];
+        $itens = [];
 
         for ($i = 0; $i < count($perfil); $i++) {
-            $item = array(
-                'perfil' => $perfil[$i],
-                'tamanho' => $tamanho[$i],
-                'cor' => $cor[$i],
-                'quantidade' => $quantidade[$i]
-            );
-            print_r($item);
-            $sql = 
-            "INSERT INTO saidas 
-                (obra_id, perfil_codigo, cor_id, tamanho, quantidade, romaneio, origem, destino, caminhao, motorista, responsavel, observacoes)
-            VALUES
-                ('$obra_id', '{$perfil[$i]}', '{$cor[$i]}', '{$tamanho[$i]}', '{$quantidade[$i]}', '$romaneio', '$origem', '$destino', '$caminhao', '$motorista', '$responsavel', '$observacoes')";
-            $res = $conn->query($sql);
+            
+            if($perfil[$i] != ""){
+                $item = array(
+                    'perfil' => $perfil[$i],
+                    'tamanho' => $tamanho[$i],
+                    'cor' => $cor[$i],
+                    'quantidade' => $quantidade[$i]
+                );
+                if($tamanho != null && $cor != null && $quantidade > 0){
+                    $itens = $item;
+                }
+            };
+            
         }
+
+        foreach($itens as $item){
+            var_dump($item);
+            echo "<br>";
+        }
+
+        // if($perfil != null)
+        // $sql = 
+        // "INSERT INTO saidas 
+        //     (obra_id, perfil_codigo, cor_id, tamanho, quantidade, romaneio, origem, destino, caminhao, motorista, responsavel, observacoes)
+        // VALUES
+        //     ('$obra_id', '{$perfil[$i]}', '{$cor[$i]}', '{$tamanho[$i]}', '{$quantidade[$i]}', '$romaneio', '$origem', '$destino', '$caminhao', '$motorista', '$responsavel', '$observacoes')";
+        // $res = $conn->query($sql);
 
         $success_message = "Saída cadastrada com sucesso!";
         $error_message = "Erro ao tentar cadastrar saída!";
@@ -53,10 +69,11 @@ try {
     case 'editar':
         $perfil = isset($_POST['perfil']) ? $_POST['perfil'] : "";
         $tamanho = isset($_POST['tamanho']) ? $_POST['tamanho'] : 6000;
-        $cor = isset($_POST['cor']) ? $_POST['cor'] : "";
-        $quantidade = isset($_POST['quantidade']) ? $_POST['quantidade'] : "";
-
-        $sql = 
+        $cor = isset($_POST['cor']) ? $_POST['cor'] : 0;
+        $quantidade = isset($_POST['quantidade']) ? $_POST['quantidade'] : 0;
+        
+        if ($perfil != "" && $cor > 0 && $quantidade > 0){
+            $sql = 
             "UPDATE saidas 
             SET 
                 obra_id = '$obra_id',
@@ -73,8 +90,7 @@ try {
                 observacoes = '$observacoes'
             WHERE id = $_REQUEST[id]
             ";
-        echo $sql;    
-        if ($perfil != "" && $cor != ""){
+
            $res = $conn->query($sql); 
         }
 
@@ -93,12 +109,12 @@ try {
         break;
   }
 
-  $res === true
-    ? print "<script>location.href = '$redirect_success?success_message=$success_message'</script>"
-    : throw new Exception("Erro na consulta SQL: " . $conn->error);
+//   $res === true
+//     ? print "<script>location.href = '$redirect_success?success_message=$success_message'</script>"
+//     : throw new Exception("Erro na consulta SQL: " . $conn->error);
     
 } catch (Exception $e) {
-  print "<script>alert('Erro: " . $e->getMessage() . "')</script>";
-  print "<script>location.href = '$redirect_error?error_message=$error_message'</script>";
+//   print "<script>alert('Erro: " . $e->getMessage() . "')</script>";
+//   print "<script>location.href = '$redirect_error?error_message=$error_message'</script>";
 }
 ?>
