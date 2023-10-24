8e8cfedef5e7a26769207ea8c09b2fb38aae3271
JoaoVittorVenturini <joaovittorventurini@outlook.com>
2023-10-24 10:43:15 -0300

    better reading mode

--- includes/navbar.php
+++ includes/navbar.php
@@ -2,25 +2,25 @@
   <nav class="navbar navbar-expand-lg">
     <ul class="navbar-nav fs-4">
       <li class="nav-item mx-4">
-        <a class="nav-link text-white-50" href="\estoque\"><i class="bi bi-house-fill"></i> Home</a>
+        <a class="nav-link text-white-50 fs-4 fw-semibold" href="\estoque\"><i class="bi bi-house-fill"></i> Home</a>
       </li>
       <li class="nav-item mx-4">
-        <a class="nav-link text-white-50" href="\estoque\pages\Entradas\read.php"><i class="bi bi-clipboard2-plus-fill"></i> Entradas</a>
+        <a class="nav-link text-white-50 fs-4 fw-semibold" href="\estoque\pages\Entradas\read.php"><i class="bi bi-clipboard2-plus-fill"></i> Entradas</a>
       </li>
       <li class="nav-item mx-4">
-        <a class="nav-link text-white-50" href="\estoque\pages\Saidas\read.php"><i class="bi bi-clipboard2-minus-fill"></i> Saídas</a>
+        <a class="nav-link text-white-50 fs-4 fw-semibold" href="\estoque\pages\Saidas\read.php"><i class="bi bi-clipboard2-minus-fill"></i> Saídas</a>
       </li>
       <li class="nav-item mx-4">
-        <a class="nav-link text-white-50" href="\estoque\pages\Obras\read.php"><i class="bi bi-building-fill"></i> Obras</a>
+        <a class="nav-link text-white-50 fs-4 fw-semibold" href="\estoque\pages\Obras\read.php"><i class="bi bi-building-fill"></i> Obras</a>
       </li>
       <li class="nav-item mx-2">
-        <a class="nav-link text-white-50" href="\estoque\pages\Clientes\read.php"><i class="bi bi-person-fill"></i> Clientes</a>
+        <a class="nav-link text-white-50 fs-4 fw-semibold" href="\estoque\pages\Clientes\read.php"><i class="bi bi-person-fill"></i> Clientes</a>
       </li>
       <li class="nav-item mx-4">
-        <a class="nav-link text-white-50" href="\estoque\pages\Perfis\read.php"><i class="bi bi-code"></i> Perfis</a>
+        <a class="nav-link text-white-50 fs-4 fw-semibold" href="\estoque\pages\Perfis\read.php"><i class="bi bi-code"></i> Perfis</a>
       </li>
       <li class="nav-item mx-4">
-        <a class="nav-link text-white-50" href="\estoque\pages\Cores\read.php"><i class="bi bi-palette2"></i> Cores</a>
+        <a class="nav-link text-white-50 fs-4 fw-semibold" href="\estoque\pages\Cores\read.php"><i class="bi bi-palette2"></i> Cores</a>
       </li>
     </ul>
   </nav>
