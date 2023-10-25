58efd64e932e450bd6bd116acf3c383da569ed91
João Vittor Venturini <92241015+JoaoVittorVenturini@users.noreply.github.com>
2023-10-25 14:40:52 -0300

    entrer filter function

--- index.php
+++ index.php
@@ -76,6 +76,13 @@
   </footer>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
   <script>
+    document.querySelector("form").addEventListener("keydown", function (event) {
+      event.keyCode === 13
+        ?filtrar(event)
+        : null;
+      
+    });
+
     function filtrar(event) {
       event.preventDefault();
       let div = document.getElementById("Items");
