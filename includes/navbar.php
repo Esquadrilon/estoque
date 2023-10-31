90b7702e9bf3b6cbd567b6a4e4fa5dfda8445fa4
JoaoVittorVenturini <joaovittorventurini@outlook.com>
2023-10-31 09:29:42 -0300

    btn go and back to other pages

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,5 +1,9 @@
-<header class="d-flex justify-content-center w-100 bg-green">
-  <nav class="navbar navbar-expand py-0 my-0">
+<header class="w-100 bg-green">
+  <nav class="navbar navbar-expand d-flex justify-content-evenly py-0 my-0">
+    <button class="btn fs-3 text-white" id="btn-volta-pagina">
+      <i class="bi bi-arrow-left"></i>
+    </button>
+
     <ul class="navbar-nav fs-4 fw-semibold d-flex flex-wrap gap-4">
       <li class="nav-item">
         <a class="nav-link text-white-50" href="\estoque\"><i class="bi bi-house-fill"></i> Home</a>
@@ -23,5 +27,18 @@
         <a class="nav-link text-white-50" href="\estoque\pages\Cores\read.php"><i class="bi bi-palette2"></i> Cores</a>
       </li>
     </ul>
+
+    <button class="btn fs-3 text-white" id="btn-avanca-pagina">
+      <i class="bi bi-arrow-right"></i>
+    </button>
   </nav>
+  <script>
+    document.getElementById('btn-volta-pagina').addEventListener('click', function() {
+      window.history.back();
+    });
+
+    document.getElementById('btn-avanca-pagina').addEventListener('click', function() {
+      window.history.forward();
+    });
+  </script>
 </header>
